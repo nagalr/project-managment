5f6fc9015beb784188b4db826f65abb1d8cb2a12
Ronnen Nagal <nagalr@gmail.com>
2020-01-20 14:06:42 -0500

    Add Requestmapping to ProjectController class to map the url: "/projects"

--- src/main/java/com/ex/pma/controllers/ProjectController.java
+++ src/main/java/com/ex/pma/controllers/ProjectController.java
@@ -1,7 +1,10 @@
 package com.ex.pma.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/projects")
 public class ProjectController {
+
 }
