88eab9896b2ac7db65a98aec531563d295948746
Ronnen Nagal <nagalr@gmail.com>
2020-01-20 14:32:34 -0500

    Add the aProject Object creation

--- src/main/java/com/ex/pma/controllers/ProjectController.java
+++ src/main/java/com/ex/pma/controllers/ProjectController.java
@@ -1,5 +1,6 @@
 package com.ex.pma.controllers;
 
+import com.ex.pma.entities.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ public class ProjectController {
     @RequestMapping("/new")
     public String displayProjectForm(Model model) {
 
+        Project aProject = new Project();
         model.addAttribute("project", aProject);
         return "new-project";
     }
