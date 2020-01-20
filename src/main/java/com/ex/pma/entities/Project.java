5e0b2891829fe697bc54feb957ba68a3e8c0cc3c
Ronnen Nagal <nagalr@gmail.com>
2020-01-20 13:24:23 -0500

    Add annotation: @GeneratedValue, @Id to the projectId field, the database will define a projectId. Define the class as an @Entity

--- src/main/java/com/ex/pma/entities/Project.java
+++ src/main/java/com/ex/pma/entities/Project.java
@@ -1,13 +1,25 @@
 package com.ex.pma.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// Defines that this is an entity, an Object from this class will convert to a record in a database
+// Each row in the database will have all the private fields of the Object
+@Entity
 public class Project {
 
+    @Id // Mention what is the generated value
+    @GeneratedValue(strategy = GenerationType.AUTO) // Generate projectId value in the database
     private long projectId;
+
     private String name;
     private String stage; // NOTSTARTED COMPLETED INPROGRESS
     private String description;
 
     public Project(){
+
     }
 
     public Project(String name, String stage, String description) {
