c12756856c396a8691c84fa590b9281472e1fce1
Ronnen Nagal <nagalr@gmail.com>
2020-01-20 14:50:50 -0500

    Create a 'saveProjectForm' function to handle the saving to the database with annotation of @PostMapping, change 'displayProjectForm' function annotation to @GetMapping to avoid sending the 'POST' action.

--- src/main/java/com/ex/pma/controllers/ProjectController.java
+++ src/main/java/com/ex/pma/controllers/ProjectController.java
@@ -3,17 +3,25 @@ package com.ex.pma.controllers;
 import com.ex.pma.entities.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
 
-    @RequestMapping("/new")
+    @GetMapping("/new")
     public String displayProjectForm(Model model) {
 
         Project aProject = new Project(); // Bind an empty Object to the form
         model.addAttribute("project", aProject); // in the html form the Object name is "project"
         return "new-project";
+    }
+
+    @PostMapping("/save")
+    public String saveProjectForm(Project project, Model model) {
+        // This method should handle saving to the database
+
     }
 }
