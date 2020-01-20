bdf5cf563a0c0aa9991371c98d5535493f6dce0c
Ronnen Nagal <nagalr@gmail.com>
2020-01-20 15:25:12 -0500

    Extends ProjectRepository Interface to CrudRepository that Implement CRUD actions

--- src/main/java/com/ex/pma/dao/ProjectRepository.java
+++ src/main/java/com/ex/pma/dao/ProjectRepository.java
@@ -1,5 +1,8 @@
 package com.ex.pma.dao;
 
-public interface ProjectRepository {
+import com.ex.pma.entities.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> { // CRUD repository to access data in the database
 
 }
