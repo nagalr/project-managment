f37c7809c288646380fd52c71cf544a647d2be3f
Ronnen Nagal <nagalr@gmail.com>
2020-01-21 10:31:11 -0500

    Add a querying the database for Projects and Employees, a table will appear in the home page with the values

--- src/main/java/com/ex/pma/controllers/HomeController.java
+++ src/main/java/com/ex/pma/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.ex.pma.controllers;
 
+import com.ex.pma.dao.EmployeesRepository;
 import com.ex.pma.dao.ProjectRepository;
+import com.ex.pma.entities.Employee;
 import com.ex.pma.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,10 +17,20 @@ public class HomeController {
     @Autowired
     ProjectRepository proRepo;
 
+    @Autowired
+    EmployeesRepository empRepo;
+
     @GetMapping("/")
     public String displayHome (Model model) {
+
+        // Querying the database for Projects
         List<Project> projects = proRepo.findAll(); // return all the projects in the db
+        List<Employee> employees = empRepo.findAll();
+
+        // Querying the database for Employees
         model.addAttribute("projectsList", projects);
+        model.addAttribute("EmployeesList", employees);
+
         return "home";
     }
 }
--- src/main/java/com/ex/pma/dao/EmployeesRepository.java
+++ src/main/java/com/ex/pma/dao/EmployeesRepository.java
@@ -3,6 +3,10 @@ package com.ex.pma.dao;
 import com.ex.pma.entities.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeesRepository extends CrudRepository<Employee, Long> {
 
+    @Override
+    List<Employee> findAll();
 }
