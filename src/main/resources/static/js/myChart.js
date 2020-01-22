7afbf00dcb2e6195eb14434b6252f5c4362fa1fc
Ronnen Nagal <nagalr@gmail.com>
2020-01-22 15:26:24 -0500

    Copy some data from chartjs.org into myChart.js for practice

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,6 +1,17 @@
 // For a pie chart
-var myPieChart = new Chart(ctx, {
+new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
-    data: data,
-    options: options
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
 });
