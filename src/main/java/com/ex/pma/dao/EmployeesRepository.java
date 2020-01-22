33180cd2dbb11ce71b47b450d92a29d541eb60a2
Ronnen Nagal <nagalr@gmail.com>
2020-01-22 17:03:45 -0500

    Add data-transfer-object (dto) Interface

--- src/main/java/com/ex/pma/dao/EmployeesRepository.java
+++ src/main/java/com/ex/pma/dao/EmployeesRepository.java
@@ -1,6 +1,8 @@
-package com.ex.pma.dao;
+package com.ex.pma.dao; // data access object
 
+import com.ex.pma.dto.EmployeeProject;
 import com.ex.pma.entities.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +11,11 @@ public interface EmployeesRepository extends CrudRepository<Employee, Long> {
 
     @Override
     List<Employee> findAll();
+
+    @Query(nativeQuery = true, value =
+            "SELECT e.first_name as firstName, e.last_name as lastName, COUNT(pe.employee_id) as projectCount " +
+            "FROM employee e left join project_employee pe ON pe.employee_id = e.employee_id " +
+            "GROUP BY e.first_name, e.last_name " +
+            "ORDER BY 3 DESC")
+    public List<EmployeeProject> employeeProjects();
 }
